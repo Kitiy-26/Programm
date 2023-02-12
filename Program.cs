58090bee0b93c93992c6800e467721cb4f296517
Katrin <katerina.shashurina@yandex.ru>
2023-02-12 22:10:28 +0300

    условие задачи

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
