174a5d60de477d63ed153a939ec631a733d52458
Katrin <katerina.shashurina@yandex.ru>
2023-02-17 15:00:18 +0300

    задача 29

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿/*
-Задача 27: (branch task_2)
-Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-452 -> 11
-82 -> 10
-9012 -> 12
+Задача 29: (branch task_3)
+Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
 */
 
-int number = ReadInt("Введите число: ");
-int len = NumberLen(number);
-SumNumbers(number, len);
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+ randomArray[i] = new Random().Next(1,9);
+ Console.Write(randomArray[i] + " ");
+}
 
 
 // Функция ввода
@@ -16,28 +20,4 @@ int ReadInt(string message)
 {
  Console.Write(message);
  return Convert.ToInt32(Console.ReadLine());
-}
-
-// Функция подсчета цифр в числе
-int NumberLen(int a)
-{
- int index = 0;
- while (a > 0)
-    {
- a /= 10;
- index++;
-    }
- return index;
-}
-
-// Функция вывода суммы цифр в числе
-void SumNumbers(int n, int len)
-{
- int sum = 0;
- for (int i = 1; i <= len; i++)
-    {
- sum += n % 10;
- n /= 10;
-    }
- Console.WriteLine(sum);
 }
