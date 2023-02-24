a267fc1362d28d5782ed1dd83707412973bb72fd
Katrin <katerina.shashurina@yandex.ru>
2023-02-24 18:52:23 +0300

    задача 68

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-int m = InputInt("Введите M:");
-int n = InputInt("Введите N:");
-Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.WriteLine("Введите два положительных числа: M и N.");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
 
 int InputInt(string output)
 {
@@ -9,9 +10,12 @@ int InputInt(string output)
     return int.Parse(Console.ReadLine());
 }
 
-int CountNaturalSum(int m, int n)
+int Akkerman(int m, int n)
 {
-    if (m == n)
-        return n;
-    return n + CountNaturalSum(m, n - 1);
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
 }
