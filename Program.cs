1a3681890d43aa7145a3a8667c385cdbfd4e682f
Katrin <katerina.shashurina@yandex.ru>
2023-02-24 18:28:47 +0300

    задача 54

--- Program.cs
+++ Program.cs
@@ -1,30 +1,56 @@
-﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
-Random random = new Random();
-int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
-for (int i = 0; i < arr.GetLength(0); i++)
+Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до изменения");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
     {
-        arr[i, j] = random.Next(1, 10);
-        Console.Write(arr[i, j] + " ");
+        for (int z = 0; z < numbers.GetLength(1) - 1; z++)
+        {
+            if (numbers[i, z] < numbers[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+            {
+                int temp = 0;
+                temp = numbers[i, z];
+                numbers[i, z] = numbers[i, z + 1];
+                numbers[i, z + 1] = temp;
+            }
+        }
     }
-    Console.WriteLine();
 }
-Console.WriteLine("---------------------------");
-Console.WriteLine(arr.GetLength(0));
-for (int j = 0; j < arr.GetLength(1); j++)
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
 {
-    double sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        sum += arr[i, j];
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
     }
-    Console.Write($"{ sum / arr.GetLength(0)} ");
 }
-Console.ReadLine();
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
