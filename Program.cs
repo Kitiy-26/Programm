c67ec5a7ef04bd42ec9321a2add409e60f550796
Katrin <katerina.shashurina@yandex.ru>
2023-02-24 17:36:18 +0300

    задача 34

--- Program.cs
+++ Program.cs
@@ -1,29 +1,27 @@
-﻿Console.WriteLine("Введите длину массива:  ");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+// [345, 897, 568, 234] => 2
+Console.WriteLine("Введите размер массива:  ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
-RandonNumbers(numbers);
-Console.WriteLine("В этом массиве: ");
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
 PrintArray(numbers);
+int count = 0;
 
-void RandonNumbers(int[] numbers)
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
 {
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < numbers.Length; i++)
     {
         numbers[i] = new Random().Next(100,1000);
     }
 }
-
-
-int count = 0;
-
-for (int x = 0; x < numbers.Length; x++)
-{
-if (numbers[x] % 2 == 0)
-count++;
-}
-Console.WriteLine($"из {numbers.Length} чисел, {count} четных");
-
-
 void PrintArray(int[] numbers)
 {
     Console.Write("[ ");
